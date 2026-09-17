110a86a2a37b4be67ab4ea4289b906aba3c40889
Student <Student@LabVM2049939.2h0fwd05qjnunpqe0leeo4pzkg.jnbx.internal.cloudapp.net>
2026-09-17 11:30:12 +0200

    Successfully run name test on Username.

--- src/test/java/com/mycompany/progpartone/UserLoginsTest.java
+++ src/test/java/com/mycompany/progpartone/UserLoginsTest.java
@@ -16,53 +16,88 @@ public class UserLoginsTest {
     public UserLoginsTest() {
     }
     
-
+    
     /**
      * Test of CheckuserName method, of class UserLogins.
      */
     @Test
-    public void testCheckuserName() {
+    public void testCheckuserNameValid() {
         System.out.println("CheckuserName");
         String userName = "kyl_1";
-        UserLogins instance = null;
+        UserLogins instance = new UserLogins("","","");
         boolean expResult = true;
         boolean result = instance.CheckuserName(userName);
         assertEquals(expResult, result);
-        
-       
+  
+    }
+    
+    @Test
+    public void testCheckuserNameInValid() {
+        System.out.println("CheckuserName");
+        String userName = "kyle!!!!!";
+        UserLogins instance =new UserLogins("","","");
+        boolean expResult = false;
+        boolean result = instance.CheckuserName(userName);
+        assertEquals(expResult, result);
+  
     }
 
     /**
      * Test of checkPhoneNumber method, of class UserLogins.
      */
     @Test
-    public void testCheckPhoneNumber() {
+    public void testCheckPhoneNumberValid() {
         System.out.println("checkPhoneNumber");
         String phoneNumber = "";
         UserLogins instance = null;
         boolean expResult = false;
         boolean result = instance.checkPhoneNumber(phoneNumber);
         assertEquals(expResult, result);
+        
     }
+    
+    @Test
+    public void testCheckPhoneNumberInvalid() {
+        System.out.println("checkPhoneNumber");
+        String phoneNumber = "";
+        UserLogins instance = null;
+        boolean expResult = false;
+        boolean result = instance.checkPhoneNumber(phoneNumber);
+        assertEquals(expResult, result);
+        
+    }
+    
 
     /**
      * Test of checkPassword method, of class UserLogins.
      */
     @Test
-    public void testCheckPassword() {
+    public void testCheckPasswordValid() {
         System.out.println("checkPassword");
         String Password = "";
         UserLogins instance = null;
         boolean expResult = false;
         boolean result = instance.checkPassword(Password);
         assertEquals(expResult, result);
+        
+    }
+    
+    @Test
+    public void testCheckPasswordInvalid() {
+        System.out.println("checkPassword");
+        String Password = "";
+        UserLogins instance = null;
+        boolean expResult = false;
+        boolean result = instance.checkPassword(Password);
+        assertEquals(expResult, result);
+        
     }
 
     /**
      * Test of registerUser method, of class UserLogins.
      */
     @Test
-    public void testRegisterUser() {
+    public void testRegisterUserValid() {
         System.out.println("registerUser");
         String userName = "";
         String phoneNumber = "";
@@ -71,6 +106,76 @@ public class UserLoginsTest {
         String expResult = "";
         String result = instance.registerUser(userName, phoneNumber, Password);
         assertEquals(expResult, result);
+        ;
+    }
+    
+    @Test
+    public void testRegisterUserInvalid() {
+        System.out.println("registerUser");
+        String userName = "";
+        String phoneNumber = "";
+        String Password = "";
+        UserLogins instance = null;
+        String expResult = "";
+        String result = instance.registerUser(userName, phoneNumber, Password);
+        assertEquals(expResult, result);
+        ;
+    }
+
+    /**
+     * Test of UserLogin method, of class UserLogins.
+     */
+    @Test
+    public void testUserLoginValid() {
+        System.out.println("UserLogin");
+        String userName = "";
+        String phoneNumber = "";
+        String Password = "";
+        UserLogins instance = null;
+        boolean expResult = false;
+        boolean result = instance.UserLogin(userName, phoneNumber, Password);
+        assertEquals(expResult, result);
+        
+    }
+    
+      @Test
+    public void testUserLoginInvalid() {
+        System.out.println("UserLogin");
+        String userName = "";
+        String phoneNumber = "";
+        String Password = "";
+        UserLogins instance = null;
+        boolean expResult = false;
+        boolean result = instance.UserLogin(userName, phoneNumber, Password);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of returnUserLoginStatus method, of class UserLogins.
+     */
+    @Test
+    public void testReturnUserLoginStatusValid() {
+        System.out.println("returnUserLoginStatus");
+        Boolean LoginSuccessful = null;
+        boolean LoginUnsuccessful = false;
+        UserLogins instance = null;
+        String expResult = "";
+        String result = instance.returnUserLoginStatus(LoginSuccessful, LoginUnsuccessful);
+        assertEquals(expResult, result);
+     
+    }
+    
+      @Test
+    public void testReturnUserLoginStatusInvalid() {
+        System.out.println("returnUserLoginStatus");
+        Boolean LoginSuccessful = null;
+        boolean LoginUnsuccessful = false;
+        UserLogins instance = null;
+        String expResult = "";
+        String result = instance.returnUserLoginStatus(LoginSuccessful, LoginUnsuccessful);
+        assertEquals(expResult, result);
+     
     }
     
 }
